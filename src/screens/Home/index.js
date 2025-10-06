211ce4d51cd576bc4aeb8b00bcfad57a0334f400
GitSA-2025 <accesssystemfatec@gmail.com>
2025-10-06 17:56:08 -0300

    Update index.js

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -8,138 +8,52 @@ import {
   ScrollView,
   Image
 } from 'react-native';
-import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { useNavigation, useFocusEffect, useRoute } from '@react-navigation/native';
 import { Feather, MaterialCommunityIcons, FontAwesome5, Entypo } from '@expo/vector-icons';
 import styles from './styles';
 import api from '../../api/api';
-import SearchInput from '../../components/SearchInput';
-
-
-const Sidebar = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
-
-  const navigation = useNavigation();
-
-  const handlerHome = () => {
-    navigation.navigate('Home');
-  };
-
-  const handleDeliveryRegister = () => {
-    navigation.navigate('DeliveryRegisterScreen');
-  };
-
-  const handleQrCodeApproval = () => {
-    navigation.navigate('QrCodeApproval');
-  };
-
-  return (
-    <View style={styles.sidebar}>
-      <View style={styles.sidebarUser}>
-        <View style={styles.userAvatar} />
-        <View>
-          <Text style={styles.userName}>Nome do usuário</Text>
-          <Text style={styles.userRole}>Cargo de atuação</Text>
-        </View>
-      </View>
-
-      <View style={styles.sidebarDivider} />
-
-      <ScrollView style={styles.sidebarMenu} contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
-        <View>
-          <TouchableOpacity style={styles.menuItem} onPress={handlerHome}>
-            <MaterialCommunityIcons name="clock-outline" size={24} color="white" />
-            <Text style={styles.menuItemText}>Registros</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.menuItem} onPress={handleDeliveryRegister}>
-            <MaterialCommunityIcons name="truck-delivery-outline" size={24} color="white" />
-            <Text style={styles.menuItemText}>Fila de entregas</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.menuItem}>
-            <MaterialCommunityIcons name="file-document-outline" size={24} color="white" />
-            <Text style={styles.menuItemText}>Relatórios</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.menuItem} onPress={handleQrCodeApproval}>
-            <MaterialCommunityIcons name="qrcode-scan" size={24} color="white" />
-            <Text style={styles.menuItemText}>Aprovação{'\n'}QrCode</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View>
-          <TouchableOpacity style={styles.menuItem}>
-            <FontAwesome5 name="user-edit" size={22} color="white" />
-            <Text style={styles.menuItemText}>Editar conta</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.menuItem}>
-            <Feather name="settings" size={24} color="white" />
-            <Text style={styles.menuItemText}>Configurações</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={[styles.menuItem, styles.logoutButton]}>
-            <Text style={styles.logoutButtonText}>Sair</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </View>
-  );
-};
-
-const Header = ({ onMenuPress }) => {
-  return (
-    <View style={[styles.header, { position: 'relative', zIndex: 100 }]}>
-      <TouchableOpacity onPress={onMenuPress} style={styles.menuButton} activeOpacity={0.7}>
-        <Feather name="menu" size={28} color="white" />
-      </TouchableOpacity>
-
-      <Text style={styles.headerTitle}>Sistema de Acesso</Text>
-
-      <SearchInput />
-
-      <View style={styles.logoContainer}>
-        <Image source={require('../../../assets/kozzy_logo_2.png')} style={{ width: '50%', height: 80 }} />
-      </View>
-    </View>
-  );
-};
-
-
+import Sidebar from '../../components/Sidebar';
+import Header from '../../components/Header';
+import { StatusBar } from 'expo-status-bar';
 
 
 export default function Home() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [entradas, setEntradas] = useState([]);
   const navigation = useNavigation();
+  const route = useRoute();
+  const { user_email } = route.params;
 
   const handlerEntryRegister = () => {
-    navigation.navigate('EntryRegister');
+    navigation.navigate('EntryRegister', { user_email: user_email });
   };
+
+
 
   useFocusEffect(
     useCallback(() => {
       let isActive = true;
 
-      const fetchEntradas = async () => {
+      const fetchEntradas = async (user_email) => {
         try {
-          const res = await api.get('/api/mobile/app/exibirEntradas');
+          const res = await api.post('/api/mobile/app/exibirEntradas', { user_email });
           if (isActive) setEntradas(res.data);
         } catch (err) {
           console.error('Erro ao carregar entradas:', err.response?.data);
         }
       };
 
-      fetchEntradas();
+      fetchEntradas(user_email);
 
       return () => {
         isActive = false;
       };
-    }, [])
+    }, [user_email])
   );
 
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar style='light'/>
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <View style={{ flex: 1, marginLeft: sidebarOpen ? 230 : 0 }}>
         <Header onMenuPress={() => setSidebarOpen(!sidebarOpen)} />
@@ -171,7 +85,6 @@ export default function Home() {
               <Text style={[styles.tableHeaderCell, styles.cellEdit]}></Text>
             </View>
 
-            {/* Linhas dinâmicas */}
             {entradas.map((item, index) => (
               <View key={index} style={styles.tableRow}>
                 <Text style={[styles.tableCell, styles.cellDate]}>{item.date}</Text>
@@ -210,7 +123,7 @@ export default function Home() {
                   {item.car_plate || 'Ñ se aplica'}
                 </Text>
 
-                <TouchableOpacity style={styles.editButton} onPress={() => { navigation.navigate('EditEntryRegister', { entry: item }); }}>
+                <TouchableOpacity style={styles.editButton} onPress={() => { navigation.navigate('EditEntryRegister', { entry: item, user_email: user_email }); }}>
                   <Feather name="edit-2" size={14} color="white" />
                   <Text style={styles.editButtonText}>Editar</Text>
                 </TouchableOpacity>
